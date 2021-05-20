74fcc9cacbeae785136739708045ca15f703f7f2
psalcedodev <psalcedodev@gmail.com>
2021-05-19 22:58:08 -0600

    new

--- admincp/modules/eventrankings/settings.php
+++ admincp/modules/eventrankings/settings.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Event Rankings
  * https://webenginecms.org/
@@ -10,145 +9,93 @@
  * @build 78769f14e4ba7592617243b8d4529015
  */
 
-function saveChanges()
-{
+function saveChanges() {
     global $_POST;
-
-    $xmlPath = __PATH_EVENTRANKINGS_ROOT__ . 'config.xml';
+	
+    $xmlPath = __PATH_EVENTRANKINGS_ROOT__.'config.xml';
     $xml = simplexml_load_file($xmlPath);
-
-    if (!is_writable($xmlPath)) throw new Exception('The configuration file is not writable.');
-
-    if (!Validator::UnsignedNumber($_POST['setting_1'])) throw new Exception('Submitted setting is not valid (results_limit)');
-    $xml->results_limit = $_POST['setting_1'];
-
-    if (!Validator::UnsignedNumber($_POST['setting_4'])) throw new Exception('Submitted setting is not valid (show_level)');
-    if (!in_array($_POST['setting_4'], array(1, 0))) throw new Exception('Submitted setting is not valid (show_level)');
-    $xml->show_level = $_POST['setting_4'];
-
-    if (!Validator::UnsignedNumber($_POST['setting_5'])) throw new Exception('Submitted setting is not valid (show_class)');
-    if (!in_array($_POST['setting_5'], array(1, 0))) throw new Exception('Submitted setting is not valid (show_class)');
-    $xml->show_class = $_POST['setting_5'];
-
-
-
-    if (!Validator::UnsignedNumber($_POST['setting_6'])) throw new Exception('Submitted setting is not valid (live_ranking)');
-    if (!in_array($_POST['setting_6'], array(1, 0))) throw new Exception('Submitted setting is not valid (live_ranking)');
-    $xml->live_ranking = $_POST['setting_6'];
-
-    $xml->excluded_players = $_POST['setting_7'];
-
+	
+	if(!is_writable($xmlPath)) throw new Exception('The configuration file is not writable.');
+	
+	if(!Validator::UnsignedNumber($_POST['setting_1'])) throw new Exception('Submitted setting is not valid (results_limit)');
+	$xml->results_limit = $_POST['setting_1'];
+	
+	if(!Validator::UnsignedNumber($_POST['setting_4'])) throw new Exception('Submitted setting is not valid (show_level)');
+	if(!in_array($_POST['setting_4'], array(1, 0))) throw new Exception('Submitted setting is not valid (show_level)');
+	$xml->show_level = $_POST['setting_4'];
+	
+	if(!Validator::UnsignedNumber($_POST['setting_5'])) throw new Exception('Submitted setting is not valid (show_class)');
+	if(!in_array($_POST['setting_5'], array(1, 0))) throw new Exception('Submitted setting is not valid (show_class)');
+	$xml->show_class = $_POST['setting_5'];
+	
+	if(!Validator::UnsignedNumber($_POST['setting_6'])) throw new Exception('Submitted setting is not valid (live_ranking)');
+	if(!in_array($_POST['setting_6'], array(1, 0))) throw new Exception('Submitted setting is not valid (live_ranking)');
+	$xml->live_ranking = $_POST['setting_6'];
+	
+	$xml->excluded_players = $_POST['setting_7'];
+	
     $save = @$xml->asXML($xmlPath);
-    if (!$save) throw new Exception('There has been an error while saving changes.');
+	if(!$save) throw new Exception('There has been an error while saving changes.');
 }
 
-//Event Ranking DB Reset
-if (check_value($_REQUEST['resetdb'])) {
-    $action = resetDB($_REQUEST['resetdb']);
-    if ($action) {
-        message('success', $_REQUEST['resetdb'] . " Reset successfull");
-    } else {
-        message('error', 'There was an error reseting the database.');
-    }
+if(check_value($_POST['submit_changes'])) {
+	try {
+		
+		saveChanges();
+		message('success', 'Settings successfully saved.');
+		
+	} catch (Exception $ex) {
+		message('error', $ex->getMessage());
+	}
 }
 
-if (check_value($_POST['submit_changes'])) {
-    try {
-
-        saveChanges();
-        message('success', 'Settings successfully saved.');
-    } catch (Exception $ex) {
-        message('error', $ex->getMessage());
-    }
-}
-
-function resetDB($dbname)
-{
-    $db = config('SQL_USE_2_DB', true) ? Connection::Database('Me_MuOnline') : Connection::Database('MuOnline');
-    $result = $db->query("DELETE FROM $dbname");
-    if (!$result) return;
-    return true;
-}
 
 // load configs
-$pluginConfig = simplexml_load_file(__PATH_EVENTRANKINGS_ROOT__ . 'config.xml');
-if (!$pluginConfig) throw new Exception('Error loading config file.');
+$pluginConfig = simplexml_load_file(__PATH_EVENTRANKINGS_ROOT__.'config.xml');
+if(!$pluginConfig) throw new Exception('Error loading config file.');
 ?>
 <h2>Event Rankings Settings</h2>
 
+<h4>Rankings Home</h4>
+<p><?php echo '<a href="'.__PATH_MODULES_RANKINGS__.'bloodcastle/" target="_blank">'.__PATH_MODULES_RANKINGS__.'bloodcastle/</a>'; ?></p>
+<p><?php echo '<a href="'.__PATH_MODULES_RANKINGS__.'devilsquare/" target="_blank">'.__PATH_MODULES_RANKINGS__.'devilsquare/</a>'; ?></p>
+<p><?php echo '<a href="'.__PATH_MODULES_RANKINGS__.'chaoscastle/" target="_blank">'.__PATH_MODULES_RANKINGS__.'chaoscastle/</a>'; ?></p>
 <form action="" method="post">
 
-    <table class="table table-striped table-bordered table-hover module_config_tables">
+	<table class="table table-striped table-bordered table-hover module_config_tables">
         <tr>
-            <th>Results Limit<br /><span>Number of players to show in the ranking.</span></th>
+            <th>Results Limit<br/><span>Number of players to show in the ranking.</span></th>
             <td>
-                <input class="form-control" type="text" name="setting_1" value="<?php echo $pluginConfig->results_limit; ?>" />
+				<input class="form-control" type="text" name="setting_1" value="<?php echo $pluginConfig->results_limit; ?>"/>
             </td>
         </tr>
         <tr>
-            <th>Show Player Level<br /><span></span></th>
+            <th>Show Player Level<br/><span></span></th>
             <td>
-                <?php enabledisableCheckboxes('setting_4', $pluginConfig->show_level, 'Enabled', 'Disabled'); ?>
+				<?php enabledisableCheckboxes('setting_4', $pluginConfig->show_level, 'Enabled', 'Disabled'); ?>
             </td>
         </tr>
         <tr>
-            <th>Show Player Class<br /><span></span></th>
+            <th>Show Player Class<br/><span></span></th>
             <td>
-                <?php enabledisableCheckboxes('setting_5', $pluginConfig->show_class, 'Enabled', 'Disabled'); ?>
+				<?php enabledisableCheckboxes('setting_5', $pluginConfig->show_class, 'Enabled', 'Disabled'); ?>
             </td>
         </tr>
         <tr>
-            <th>Enable Live Ranking Data<br /><span>If enabled, cache will not be used and ranking data will be loaded directly from the database on each request.</span></th>
+            <th>Enable Live Ranking Data<br/><span>If enabled, cache will not be used and ranking data will be loaded directly from the database on each request.</span></th>
             <td>
-                <?php enabledisableCheckboxes('setting_6', $pluginConfig->live_ranking, 'Enabled', 'Disabled'); ?>
+				<?php enabledisableCheckboxes('setting_6', $pluginConfig->live_ranking, 'Enabled', 'Disabled'); ?>
             </td>
         </tr>
         <tr>
-            <th>Excluded Players<br /><span>Separated by comma.<br /><br />Example:<br />player1,player2,player3</span></th>
+            <th>Excluded Players<br/><span>Separated by comma.<br /><br />Example:<br />player1,player2,player3</span></th>
             <td>
-                <input class="form-control" type="text" name="setting_7" value="<?php echo $pluginConfig->excluded_players; ?>" />
+				<input class="form-control" type="text" name="setting_7" value="<?php echo $pluginConfig->excluded_players; ?>"/>
             </td>
         </tr>
-
-        <tr>
-            <td colspan="2"><input type="submit" name="submit_changes" value="Save Changes" class="btn btn-success" /></td>
+		
+		<tr>
+            <td colspan="2"><input type="submit" name="submit_changes" value="Save Changes" class="btn btn-success"/></td>
         </tr>
-    </table>
-</form>
-
-<hr>
-<h3>Reset Databases</h3>
-<form action="index.php?module=eventrankings&page=settings" method="post">
-    <table class="table table-striped table-bordered table-hover module_config_tables">
-        <tr>
-            <th>Database</th>
-            <th>Action</th>
-        </tr>
-        <tr>
-            <th>Reset Blood Castle<br /><span>If clicked, all data from Blood Castle Ranking will be deleted.</span></th>
-            <td>
-                <a href="index.php?module=eventrankings&page=settings&resetdb=RankingBloodCastle" class="btn btn-danger">Reset</a>
-                <a href="?module=cachemanager&amp;action=clear&amp;file=rankings_bloodcastle.cache" class="btn btn-danger">clear cache</a>
-            </td>
-        </tr>
-        <!-- index.php?module=modules_manager&config=downloads -->
-        <!-- index.php?module=eventrankings&page=settings&resetdb=RankingBloodCastle -->
-        <!-- index.php?module=modules_manager&config=downloads&deletelink=10 -->
-        <tr>
-            <th>Reset Chaos Castle<br /><span>If clicked, all data from Chaos Castle Ranking will be deleted.</span></th>
-            <td>
-
-                <a href="index.php?module=eventrankings&page=settings&resetdb=RankingChaosCastle" class="btn btn-danger">Reset</a>
-                <a href="?module=cachemanager&amp;action=clear&amp;file=rankings_chaoscastle.cache" class="btn btn-danger">clear cache</a>
-            </td>
-        </tr>
-        <tr>
-            <th>Reset Devil Square<br /><span>If clicked, all data from Devil Square Ranking will be deleted.</span></th>
-            <td>
-                <a href="index.php?module=eventrankings&page=settings&resetdb=RankingDevilSquare" class="btn btn-danger">Reset</a>
-                <a href="?module=cachemanager&amp;action=clear&amp;file=rankings_devilsquare.cache" class="btn btn-danger">clear cache</a>
-            </td>
-        </tr>
-
     </table>
 </form>
--- includes/plugins/eventrankings/languages/en/language.php
+++ includes/plugins/eventrankings/languages/en/language.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Event Rankings
  * https://webenginecms.org/
@@ -18,8 +17,6 @@ $lang['eventrankings_txt_1'] = 'Class';
 $lang['eventrankings_txt_2'] = 'Name';
 $lang['eventrankings_txt_3'] = 'Level';
 $lang['eventrankings_txt_4'] = 'Score';
-$lang['eventrankings_txt_5'] = 'Event Level';
-$lang['eventrankings_txt_6'] = 'General Ranking';
 
 $lang['eventrankings_error_1'] = 'Could not load top ranking class.';
 $lang['eventrankings_error_2'] = 'There are no ranking results available at the moment.';
@@ -27,30 +24,3 @@ $lang['eventrankings_error_3'] = 'Could not add top ranking cron.';
 $lang['eventrankings_error_4'] = 'Could not top ranking module.';
 $lang['eventrankings_error_5'] = 'Could not top ranking configs.';
 $lang['eventrankings_error_6'] = 'The provided ranking type is not valid.';
-
-//Blood Castle
-$lang['eventrankings_bc_1'] = 'Blood Castle 1';
-$lang['eventrankings_bc_2'] = 'Blood Castle 2';
-$lang['eventrankings_bc_3'] = 'Blood Castle 3';
-$lang['eventrankings_bc_4'] = 'Blood Castle 4';
-$lang['eventrankings_bc_5'] = 'Blood Castle 5';
-$lang['eventrankings_bc_6'] = 'Blood Castle 6';
-$lang['eventrankings_bc_7'] = 'Blood Castle 7';
-
-//Chaos Castle
-$lang['eventrankings_cc_1'] = 'Chaos Castle 1';
-$lang['eventrankings_cc_2'] = 'Chaos Castle 2';
-$lang['eventrankings_cc_3'] = 'Chaos Castle 3';
-$lang['eventrankings_cc_4'] = 'Chaos Castle 4';
-$lang['eventrankings_cc_5'] = 'Chaos Castle 5';
-$lang['eventrankings_cc_6'] = 'Chaos Castle 6';
-$lang['eventrankings_cc_7'] = 'Chaos Castle 7';
-
-//Devil Square
-$lang['eventrankings_ds_1'] = 'Devil Square 1';
-$lang['eventrankings_ds_2'] = 'Devil Square 2';
-$lang['eventrankings_ds_3'] = 'Devil Square 3';
-$lang['eventrankings_ds_4'] = 'Devil Square 4';
-$lang['eventrankings_ds_5'] = 'Devil Square 5';
-$lang['eventrankings_ds_6'] = 'Devil Square 6';
-$lang['eventrankings_ds_7'] = 'Devil Square 7';
